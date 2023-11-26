a0c174c6df12bba7d45141abfd20ba0108baad22
galang permadi <blackroserival@gmail.com>
2023-11-26 21:54:13 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui/button"
+import React, { useState } from 'react';
+import { account, ID } from './lib/appwrite';
+
+const App = () => {
+  const [loggedInUser, setLoggedInUser] = useState({name: ''});
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  async function login(email:string, password:string) {
+    await account.createEmailSession(email, password);
+    setLoggedInUser(await account.get());
+  }
+
+  return (
+    <div>
+      <p>
+        {loggedInUser ? `Logged in as ${loggedInUser.name}` : 'Not logged in'}
+      </p>
+
+      <form>
+        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
+
+        <Button type="button" onClick={() => login(email, password)}>
+          Login
+        </Button>
+
+        <Button
+          type="button"
+          onClick={async () => {
+            await account.create(ID.unique(), email, password, name);
+            login(email, password);
+          }}
+        >
+          Register
+        </Button>
+
+        <Button
+          type="button"
+          onClick={async () => {
+            await account.deleteSession('current');
+            setLoggedInUser({name: ''});
+          }}
+        >
+          Logout
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
+
